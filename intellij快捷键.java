8433a6d55132a8306cbb7fd41b81f9c7fbfec5b3
volibearGeek <liuyongtao@imxiaomai.com>
2019-02-20 21:02:52 +0800

    快捷键

---
+++ intellij快捷键.java
@@ -0,0 +1,79 @@
+================================================
+Alt+Insert:新建类
+Alt+Enter:帮助提示
+Alt+Shift+左键多个位置编辑
+F2:依次查看错误
+ctrl+w:增量选择
+ctrl+d:复制一行
+ctrl+alt+h:调用这个方法都有哪些地方
+ctrl+alt+b:进入接口实现类
+ctrl+u:定位到类的父类或者父接口
+ctrl+F12:显示类结构图
+shift+click:关闭当前文件
+Ctrl＋N:可以快速打开类
+Ctrl＋Alt＋V: 生成变量
+alt+下箭头:可以快速移动代码
+ctrl+o:选择父类的方法进行重写
+Ctrl+Alt+L:格式化代码
+Ctrl＋Alt＋T:可以把代码包在一块内，例如try/catch
+ctrl+shift+f:全局搜索
+Ctrl＋F7:可以查询当前所有的相同元素，然后按F3可以选择
+alt+f7:查询该变量在当前文件所有位置引用及数量
+Alt+F3:逐个往下查找相同文本，并高亮显示
+ctrl+F3:跳转到词的下个引用位置
+shift + F6:批量修改变量名
+alt + j :增量选择下个相同单词
+ctrl + alt + 左方向键: 退回到上次编辑位置
+Ctrl+Shift+Space:自动补全代码
+ctrl+shift+M:选中对应括号
+Ctrl+Shift+Backspace:回到最后编辑位置
+Alt+Shift+Up/Down:上/下移一行
+Ctrl+Shift+F7:高亮显示所有该文本
+Alt+F3:逐个往下查找相同文本，并高亮显示
+alt+7:查看structure视图
+ctrl+H:类继承机构
+ctrl+alt+H:类和方法被调用位置
+ctrl+shift+enter:帮你自动填充分号
+Ctrl+Alt+O:优化导入的类和包
+f11或者ctrl+f11:插入书签
+shift+f11:显示所有的书签
+
+
+
+Ctrl+Alt+I，将选中的代码进行自动缩进编排，这个功能在编辑 JSP 文件时也可以工作
+Ctrl+空格，代码提示（与系统输入法快捷键冲突）
+Ctrl+Shift+Alt+N，查找类中的方法或变量
+Alt+Shift+C，最近的更改
+自动补全属性名称 : Ctrl + Alt + f
+itar 生成array 
+iter 生成增强forxun
+itli 生成List的遍历
+itit  生成iterator 迭代(while)
+
+
+ctrl + i  实现接口的方法
+Alt+回车 导入包,自动修正
+Ctrl+Shift+N 查找文件
+Ctrl+R 替换文本
+Ctrl+Alt+Space 类名或接口名提示
+Ctrl+P 方法参数提示
+Ctrl+Shift+Alt+N 查找类中的方法或变量
+Alt+Shift+C 对比最近修改的代码
+
+Ctrl+Alt+H打开方法调用结构    >>> 
+
+Ctrl+J  自动代码
+Ctrl+H 显示类结构图
+Alt+F1 查找代码所在位置
+Ctrl+Alt+ left/right 返回至上次浏览的位置
+Alt+ left/right 切换代码视图
+Alt+ Up/Down 在方法间快速移动定位
+Ctrl+Shift+Up/Down 代码向上/下移动。
+ 
+=================================
+Ctrl+H 显示类结构图
+Alt+F1 查找代码所在位置
+Alt+1 快速打开或隐藏工程面板
+Ctrl+Alt+ left/right 返回至上次浏览的位置
+Alt+ left/right 切换代码视图
+Alt+ Up/Down 在方法间快速移动定位111
---
+++ vs_shortcut.md
@@ -0,0 +1,13 @@
+* 删除                                         Ctrl + Shift + K
+* 复制当前行                                    Alt + Shift + up
+* 选取多个相同单词                               Ctrl + D
+* 切换不同文件                                  Ctrl + table 
+* 选中一行                                      Ctrl + i
+* 多处任意位置修改                               Alt + Click
+* Markdown预览切换                              Ctrl + Shift + V
+* 在鼠标拖动区域的每行行尾插入光标                 Shift + Alt + (drag mouse) 
+* 回到最后一次编辑处                              ctlr + k + q(改为ctrl+q)
+* 切换不同视图                                  ctrl + q 
+* 选中所有相同单词                              Ctrl + Shift + L
+
+
