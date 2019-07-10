b10b494ddf94b6e5885ee3105b95f4f449964c3f
volibearGeek <liuyongtao@imxiaomai.com>
2019-07-10 16:17:55 +0800

    kafka脚本优化

--- shell/start-kafka.sh
+++ shell/start-kafka.sh
@@ -3,6 +3,9 @@ echo "=================	kafka cluster starting	================="
 for i in 01 02 03
 do
 ssh learn$i "source /etc/profile;/opt/module/kafka/bin/kafka-server-start.sh /opt/module/kafka/config/server.properties  > /root/logs/kfk_start.log 2>&1 &"
+if [[ $? -eq 0 ]];then
+echo "start kafka on learn $i is down"
+fi
 done
-echo "=================	kafka cluster end  ======================="
-echo $?
+echo "=================   all kafka started   ======================="
+exit 0
--- shell/stop-kafka.sh
+++ shell/stop-kafka.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 echo "=================	kafka cluster stopping	================="
+APP_NAME=kakfa
 for i in 01 02 03
 do
 ssh learn$i "source /etc/profile;/opt/module/kafka/bin/kafka-server-stop.sh  > /root/logs/kfk_stop.log 2>&1 &"
+if [[ $? -eq 0 ]]; then
+echo "stop $APP_NAME on learn$i is down"
+fi
 done
-echo "=================       kafka cluster end            ======================="
-echo $?
+echo "=================       all kafka stopped            ======================="
+exit 0
