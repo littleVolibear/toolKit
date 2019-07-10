1ed8db4890bbdef6fc095e1104fb8fcd2b6f1052
volibearGeek <liuyongtao@imxiaomai.com>
2019-07-10 15:57:37 +0800

    zk-shell 更新、软件列表更新

--- shell/start-zk.sh
+++ shell/start-zk.sh
@@ -1,6 +1,9 @@
-#!/bin/sh
-echo "zkServer start"
+#!/bin/bash
+echo "===============   zkServer start   ========================"
 for i in 01 02 03
 do
-ssh learn$i "source /etc/profile;/opt/module/zookeeper-3.4.10/bin/zkServer.sh start"
+ssh learn$i "source /etc/profile;/opt/module/zookeeper-3.4.10/bin/zkServer.sh start >/root/logs/zk-start.log 2>&1;exit"
+if [[ $? -eq 0 ]];then
+echo "starting kafka on learn$i is down"
+fi
 done
--- shell/stop-zk.sh
+++ shell/stop-zk.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
-echo "zkServer stop"
+echo "===============   zkServer stop   ==========================="
 for i in 01 02 03
 do
-ssh learn$i "source /etc/profile;/opt/module/zookeeper-3.4.10/bin/zkServer.sh stop"
+ssh learn$i "source /etc/profile;/opt/module/zookeeper-3.4.10/bin/zkServer.sh stop > /root/logs/stop-zk.log 2>&1;exit"
+if [[ $? -eq 0 ]] ; then
+echo "stopping zk on learn$i is down"
+fi
 done
+echo "all zk are stopped"
+exit 0
