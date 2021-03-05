29d128cefee39494b85a804bba02654fed3e7d64
liuyt15 <liuyt15@lenovo.com>
2021-03-05 21:36:29 +0800

    1

---
+++ Constants.java
@@ -0,0 +1,57 @@
+package com.lenovo.mbg.util;
+
+public class Constants {
+	/**
+	 * API response status code
+	 */
+	public static final String CODE_SUCCESS = "200";
+	public static final String CODE_ERROR = "500";
+	public static final String CODE_UNAUTHORIZED = "401";
+	
+    public static final String BR = "<br/>&nbsp;&nbsp;&nbsp;&nbsp;";
+    public static final String EMPTYKEY = " is empty.";
+    public static final String AND = " and ";
+    public static final String ALREADYEXISTSKEY = " already exists in the system.";
+    public static final String ADD="ADD";
+    public static final String DEL="DELETE";
+    public static final String UPDATE="UPDATE";
+    public static final String END_MARK="#EOF";
+    public static final String USERSESSION="USERSESSION";
+    public static final String ISADMIN_Y="Y";
+    public static final String ISADMIN_N="N";
+    public static final String SUPER_ADMINISTRATOR_NAME="admin";
+    public static final String SPLIT=",";
+    public static final String USBKEY="1F2C6A90703876B147731CLR8ZCEDD3D";
+    public static final String TAG_EOF  = "#EOF";
+    
+    // bom history Loadsheet
+    public static final String BOMHISTORY_LOADSHEET = "BOMHistory_Loadsheet.xlsx";
+ 	public static final String BOMHISTORY_LOADSHEET_PATH = "templates/report/BOMHistory_Loadsheet.xlsx";
+ 	
+ 	//MBOMImpStatusSheet
+ 	public static final String MBOMIMPLEMENT_STATUS_REPORT = "MBOMImpStatusSheet.xlsx";
+ 	public static final String MBOMIMPLEMENT_STATUS_REPORT_PATH = "templates/report/MBOMImpStatusSheet.xlsx";
+ 	
+ 	//SalesmodelMatrixReport
+ 	public static final String SALESMODELMATRIX_REPORT = "SalesmodelMatrixReport.xlsx";
+ 	public static final String SALESMODELMATRIX_PATH = "templates/report/SalesmodelMatrixReport.xlsx";
+ 	
+ 	//WorkflowStatusTrackingReport
+ 	public static final String WFSTATUS_REPORT = "WorkflowStatusTrackingReport.xlsx";
+ 	public static final String WFSTATUS_PATH = "templates/report/WorkflowStatusTrackingReport.xlsx";
+ 	
+ 	//GCMECTrackingReport
+ 	public static final String GCMEC_REPORT = "ComponentPublishTimeTracingDetailReport.xlsx";
+ 	public static final String GCMEC_PATH = "templates/report/ComponentPublishTimeTracingDetailReport.xlsx";
+ 	
+ 	//BOM Qual Report
+ 	public static final String BOMQUALTRACKING_LOADSHEET = "BOMQualTrackingReportTemplate.xlsx";
+ 	public static final String BOMQUALTRACKING_LOADSHEET_PATH = "templates/report/BOMQualTrackingReportTemplate.xlsx";
+
+
+	//Npi Mp Lt Report
+	public static final String NPIMPLT_REPORT = "NpiMpLtReport.xlsx";
+	public static final String NPIMPLT_PATH = "templates/report/NpiMpLtReport.xlsx";
+
+
+}
